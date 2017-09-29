3bf8b58ba35ffcf425c3d15f10ab00e28d42f8dd
AliaksandraBayanouskaya <aliaksandra_bayanouskaya@epam.com>
2017-09-29 12:51:52 +0300

    first commit

---
+++ hello/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.hello;
+
+public class HelloWorld {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World555");
+        System.out.println("Hello World555");
+    }
+
+}
