b5bf47e7358d4bbc60c308a2c80887206b8141f3
AliaksandraBayanouskaya <aliaksandra_bayanouskaya@epam.com>
2017-10-02 12:00:59 +0300

    5 commit

--- hello/src/main/java/hello/HelloWorld.java
+++ hello/src/main/java/hello/HelloWorld.java
@@ -3,9 +3,8 @@ package main.java.hello;
 public class HelloWorld {
 
     public static void main(final String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World555");
-        System.out.println("Hello World555");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
     }
 
 }
